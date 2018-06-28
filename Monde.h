eb81a236c45495fe47fff385fbf9ed3751219c63
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-06-28 22:26:03 +0200

    Fonctions d'ajout de vaisseau et de bonus

---
+++ Monde.h
@@ -0,0 +1,30 @@
+/* Auteurs: LOPES MENDES Ailton
+*  Creation: 26/06/2018
+*  Modification: 26/06/2018*/
+#ifndef __MONDE__
+#define __MONDE__
+
+#include "Vaisseaux.h"
+#include "Bonus.h"
+
+#define NB_BOTS_DEBUT 0
+#define BLOC_VAISSEAUX 16 /* Bloc de mémoire alloué au début pour les vaisseaux. */
+#define BLOC_BONUS 8 /* Bloc de mémoire alloué au début pour les bonus. */
+
+typedef struct {
+	Vaisseau* vaisseaux;
+	int nb_vaisseaux;
+	int nb_vaisseaux_max;
+	Bonus* bonus;
+	int nb_bonus;
+	int nb_bonus_max;
+} Monde;
+
+/* Initialise un monde par adresse, avec le vaisseau du joueur + NB_BOTS_DEBUT bots ainsi que 0 bonus. */
+void initialiser_monde(Monde* monde);
+/* Ajoute un vaisseau au monde `monde`. */
+void ajouter_vaisseau_monde(Monde* monde);
+/* Ajoute un bonus au monde `monde`. */
+void ajouter_bonus_monde(Monde* monde, Bonus bonus);
+
+#endif
