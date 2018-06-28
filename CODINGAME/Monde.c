edfa8b0415a4fb302b638fde5001a9f92bf5f806
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-06-28 22:27:07 +0200

    Fonctions d'ajout de vaisseau et de bonus

--- CODINGAME/Monde.c
+++ CODINGAME/Monde.c
@@ -18,3 +18,35 @@ void initialiser_monde(Monde* monde) {
 
 	monde->nb_bonus = 0;
 }
+
+void ajouter_vaisseau_monde(Monde* monde) {
+	Vaisseau bot_defaut = {nb_vaisseaux, nb_vaisseaux, BOT, STOP, LENT, RIEN};
+
+	assert(NULL != monde);
+
+	/* Si on a atteint la capacité maximum de vaisseau, on la double. */
+	if ( monde->nb_vaisseaux >= monde->nb_vaisseaux_max ) {
+		monde->nb_vaisseaux_max *= 2;
+		monde->vaisseaux = (Vaisseau*)realloc(monde->vaisseaux, sizeof(Vaisseau) * monde->nb_vaisseaux_max);
+		if ( NULL == monde->vaisseaux )
+			exit(EXIT_FAILURE);
+	}
+
+	monde->vaisseaux[monde->nb_vaisseaux] = bot_defaut;
+	monde->nb_vaisseaux += 1;
+}
+
+void ajouter_bonus_monde(Monde* monde, Bonus bonus) {
+	assert(NULL != monde);
+
+	/* Si on a atteint la capacité maximum de bonus, on la double. */
+	if ( monde->nb_bonus >= monde->nb_bonus_max ) {
+		monde->nb_bonus_max *= 2;
+		monde->bonus = (Bonus*)realloc(monde->bonus, sizeof(Bonus) * monde->nb_bonus_max);
+		if ( NULL == monde->bonus )
+			exit(EXIT_FAILURE);
+	}
+
+	monde->bonus[monde->nb_bonus] = bonus;
+	monde->nb_bonus += 1;
+}
--- CODINGAME/Monde.h
+++ CODINGAME/Monde.h
@@ -14,11 +14,17 @@
 typedef struct {
 	Vaisseau* vaisseaux;
 	int nb_vaisseaux;
+	int nb_vaisseaux_max;
 	Bonus* bonus;
 	int nb_bonus;
+	int nb_bonus_max;
 } Monde;
 
 /* Initialise un monde par adresse, avec le vaisseau du joueur + NB_BOTS_DEBUT bots ainsi que 0 bonus. */
 void initialiser_monde(Monde* monde);
+/* Ajoute un vaisseau au monde `monde`. */
+void ajouter_vaisseau_monde(Monde* monde);
+/* Ajoute un bonus au monde `monde`. */
+void ajouter_bonus_monde(Monde* monde, Bonus bonus);
 
 #endif
