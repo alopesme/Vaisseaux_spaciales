d531b2931ddda74a74e7a256a430e9a0b33a8e8c
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-06-28 20:22:07 +0200

    Ajout des bonus
    Le nom des bonus sera à changer au fur et à mesure (BONUS1, BONUS2 ...)

---
+++ CODINGAME/Bonus.h
@@ -0,0 +1,13 @@
+/* Auteurs: LOPES MENDES Ailton
+*  Creation: 26/06/2018
+*  Modification: 26/06/2018*/
+#ifndef __BONUS__
+#define __BONUS__
+
+typedef enum {
+	BONUS1,
+	BONUS2,
+	BONUS3,
+} Bonus;
+
+#endif
