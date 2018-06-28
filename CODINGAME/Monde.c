3b10b805f28480b7dc237fa7a0eb440132731d80
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-06-28 20:23:19 +0200

    Initialisation d'un monde
    Un monde est composé d'un tableau de vaisseaux, et d'un tableau de bonus.

--- CODINGAME/Monde.c
+++ CODINGAME/Monde.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "Monde.h"
+
+void initialiser_monde(Monde* monde) {
+	assert(NULL != monde);
+	assert(BLOC_VAISSEAUX > NB_BOTS_DEBUT);
+
+	monde->vaisseaux = allouer_vaisseaux(BLOC_VAISSEAUX);
+	monde->nb_vaisseaux = 1 + NB_BOTS_DEBUT;
+
+	monde->bonus = (Bonus*)malloc(sizeof(Bonus) * BLOC_BONUS);
+	if (monde->bonus == NULL) {
+		printf("Malloc rate \t\tinitialiser_monde -> bonus\n");
+		exit(EXIT_FAILURE);
+	}
+
+	monde->nb_bonus = 0;
+}
