07fc577bf60fc9daec17295e2090da9103bd14dd
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-06-29 16:57:16 +0200

    Add files via upload

--- CODINGAME/Main.c
+++ CODINGAME/Main.c
@@ -8,31 +8,22 @@
 #include "Vaisseaux.h"
 #include "Monde.h"
 #include "Test.h"
+#include "interface_graphique.h"
 #include <MLV/MLV_all.h>
 
 int main(int argc, char const *argv[]) {
-	/*int x, y;
-	Coord_Tir coord_t;
+	Monde monde;
 
-	MLV_create_window("test", "test", 600, 600);
+	initialiser_monde(&monde);
 
-	MLV_wait_mouse(&x, &y);
+	MLV_create_window("Wesh zoulette", "Bien ou quoi", monde.largeur_fenetre, monde.hauteur_fenetre);
 
-	init_coord_tir(600 / 2, 600 - 20, &coord_t);
+	afficher_background();
 
-	while ((coord_t.tir_x < 600 || coord_t.tir_y < 600)
-		|| (coord_t.tir_x > 0 || coord_t.tir_y > 0)) {
-		MLV_clear_window(MLV_COLOR_BLACK);
+	MLV_actualise_window();
+	MLV_wait_mouse(NULL, NULL);
 
-		MLV_draw_circle(coord_t.tir_x, coord_t.tir_y, 5, MLV_COLOR_RED);
-		calculer_tir(&coord_t, x, y);
-
-		MLV_actualise_window();
-	}
-
-	MLV_free_window();*/
-
-
+	MLV_free_window();
 
 	return 0;
 }
---
+++ CODINGAME/interface_graphique.h
@@ -0,0 +1,9 @@
+#ifndef __INTERFACE_GRAPHIQUE__
+#define __INTERFACE_GRAPHIQUE__
+
+#define FOND MLV_rgba(0, 8, 84, 255)
+
+/* Affiche le background du jeu. */
+void afficher_background();
+
+#endif
