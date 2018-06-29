8d8ec080022db2a3820a7beac5ebe1fb3062af9a
alopesme <40667851+alopesme@users.noreply.github.com>
2018-06-29 17:10:44 +0200

    Delete interface_graphique.h

--- CODINGAME/interface_graphique.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INTERFACE_GRAPHIQUE__
-#define __INTERFACE_GRAPHIQUE__
-
-#define FOND MLV_rgba(0, 8, 84, 255)
-
-/* Affiche le background du jeu. */
-void afficher_background();
-
-#endif
