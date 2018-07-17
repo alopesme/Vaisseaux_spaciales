db1ccc6986fbe3ebb297ea550dd4429437a11272
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-07-17 20:14:46 +0200

    Update Monde.c

--- VAIS_JEU/Monde.c
+++ VAIS_JEU/Monde.c
@@ -44,7 +44,7 @@ void initialiser_monde(Monde* monde, const int t_x, const int t_y) {
     }
 
     monde->vaisseaux = allouer_vaisseaux(BLOC_VAISSEAUX);
-    monde->tab[t_x][t_y].indice = 0;
+    monde->tab[t_y / 2][t_x / 2].indice = 0;
     monde->nb_vaisseaux_max = BLOC_VAISSEAUX;
     monde->nb_vaisseaux = 1;
 
