78618633f16c42e0cf1af9d2b34e29262f83a07b
SaladTomatOignon <34814015+SaladTomatOignon@users.noreply.github.com>
2018-07-17 20:36:57 +0200

    Update Monde.c

--- VAIS_JEU/Monde.c
+++ VAIS_JEU/Monde.c
@@ -90,9 +90,9 @@ void ajouter_vaisseau_monde(Monde* monde, Vaisseau vaisseau, int x, int y, Etats
             exit(EXIT_FAILURE);
     }
 
-    monde->tab[x][y].etats = type;
-    monde->tab[x][y].vie = vie;
-    monde->tab[x][y].indice = monde->nb_vaisseaux;
+    monde->tab[y][x].etats = type;
+    monde->tab[y][x].vie = vie;
+    monde->tab[y][x].indice = monde->nb_vaisseaux;
 
     monde->nb_vaisseaux += 1;
 }
@@ -109,4 +109,78 @@ void libere_monde(Monde* monde) {
     free(monde->tab);
     monde->tab = NULL;
 
+}
+
+void calculer_prochaine_case_vaisseau(int x, int y, Vaisseau v, int* out_x, int* out_y) {
+    assert(NULL != out_x);
+    assert(NULL != out_y);
+
+    switch ( v.dep ) {
+        case NORD:
+            *out_x = x;
+            *out_y -= v.vi;
+        case OUEST:
+            *out_x += v.vi;
+            *out_y = y;
+        case SUD:
+            *out_x = x;
+            *out_y += v.vi;
+        case EST:
+            *out_x -= v.vi;
+            *out_y = y;
+        default:
+            *out_x = -1;
+            *out_y = -1;
+    }
+}
+
+int peut_se_deplacer(Monde* monde, int x, int y, int indice_vaisseau) {
+    int i;
+    int nouvelle_case_x, nouvelle_case_y;
+
+    assert(NULL != monde);
+    assert(indice_vaisseau >= 0);
+
+    /* On vérifie si on ne va pas en hors map. */
+    calculer_prochaine_case_vaisseau(x, y, monde->vaisseaux[indice_vaisseau], &nouvelle_case_x, &nouvelle_case_y);
+    if ( nouvelle_case_x < 0 || nouvelle_case_x > monde->taille_x ||
+         nouvelle_case_y < 0 || nouvelle_case_y > monde->taille_y )
+
+        return 0;
+
+
+    /* On regarde case par case si il n'y a pas quelque chose qui barre la route du vaisseau. */
+    switch ( monde->vaisseaux[indice_vaisseau].dep ) {
+        case NORD: for (i=1; i < monde->vaisseaux[indice_vaisseau].vi + 1; i++) {
+            if ( monde->tab[y - i][x].etats != VIDE ) return 0; }
+            break;
+        case OUEST: for (i=1; i < monde->vaisseaux[indice_vaisseau].vi + 1; i++) {
+            if ( monde->tab[y][x + i].etats != VIDE ) return 0; }
+            break;
+        case SUD: for (i=1; i < monde->vaisseaux[indice_vaisseau].vi + 1; i++) {
+            if ( monde->tab[y + i][x].etats != VIDE ) return 0; }
+            break;
+        case EST: for (i=1; i < monde->vaisseaux[indice_vaisseau].vi + 1; i++) {
+            if ( monde->tab[y][x - i].etats != VIDE ) return 0; }
+            break;
+        default:
+            return 0;
+    }
+
+    return 1;
+}
+
+void deplacer_vaisseau(Monde* monde, int x, int y) {
+    int nouvelle_case_x, nouvelle_case_y;
+
+    assert(NULL != monde);
+
+    calculer_prochaine_case_vaisseau(x, y, monde->vaisseaux[monde->tab[y][x].indice], &nouvelle_case_x, &nouvelle_case_y);
+
+    /* On déplace le vaisseau vers sa nouvelle case. */
+    monde->tab[nouvelle_case_y][nouvelle_case_x] = monde->tab[y][x];
+    /* Son ancienne case devient libre. */
+    monde->tab[y][x].etats = VIDE;
+    monde->tab[y][x].vie = 0;
+    monde->tab[y][x].indice = -1;
 }
