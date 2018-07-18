ee615492d4443d0e485fa6acd01bf37678c35d36
alopesme <40667851+alopesme@users.noreply.github.com>
2018-07-18 18:50:13 +0200

    Add files via upload

--- VAIS_JEU/Jeu.c
+++ VAIS_JEU/Jeu.c
@@ -6,48 +6,81 @@
 #include "Interface_graphique.h"
 #include "Monde.h"
 
-static void action_element(Monde* monde, int x, int y, int* a_bouge) {
-	int i_vaisseau;
-
-	assert(NULL != monde);
-	assert(NULL != a_bouge);
+static void matiere_monde(Monde *mo, const int x, const int y) {
+	assert(NULL != mo);
 	assert(x >= 0);
+	assert(x < mo->taille_x);
 	assert(y >= 0);
+	assert(y < mo->taille_y);
 
-	switch ( monde->tab[y][x].etats ) {
+	switch (mo->tab[y][x].etats) {
 		case VIDE:
 			break;
 		/* Si c'est un mur ou un obstacle, on vérifie sa vie, et si elle est négative on le détruit. */
 		case MUR:
 		case OBSTACLE:
-			if ( monde->tab[y][x].vie <= 0 )
-				monde->tab[y][x].etats = VIDE;
+			if (mo->tab[y][x].vie <= 0)
+				mo->tab[y][x].etats = VIDE;
+				break;
+					/* Si c'est un tir, ... */
+		default:
 			break;
-		/* Si c'est un tir, ... */
-		case TIR:
-			break;
-		/* Si c'est un vaisseau, on le déplace si nécessaire. */
+	}
+}
+
+static void tir_monde(Monde *mo, const int x, const int y) {
+	assert(NULL != mo);
+	assert(x >= 0);
+	assert(x < mo->taille_x);
+	assert(y >= 0);
+	assert(y < mo->taille_y);
+
+	switch (mo->tab[y][x].etats) {
+		case TIR: break;
+		default: break;
+	}
+}
+
+static void vaisseaux_monde(Monde *mo, const int x, const int y, int *a_bouge) {
+	int i_vaisseau;
+	assert(NULL != mo);
+	assert(x >= 0);
+	assert(x < mo->taille_x);
+	assert(y >= 0);
+	assert(y < mo->taille_y);
+	switch (mo->tab[y][x].etats) {
 		case JOUEUR:
 			if ( MLV_get_keyboard_state(MLV_KEYBOARD_z) == MLV_PRESSED )
-				monde->vaisseaux[0].dep = NORD;
+				mo->vaisseaux[0].dep = NORD;
 			if ( MLV_get_keyboard_state(MLV_KEYBOARD_d) == MLV_PRESSED )
-				monde->vaisseaux[0].dep = OUEST;
+				mo->vaisseaux[0].dep = OUEST;
 			if ( MLV_get_keyboard_state(MLV_KEYBOARD_s) == MLV_PRESSED )
-				monde->vaisseaux[0].dep = SUD;
+				mo->vaisseaux[0].dep = SUD;
 			if ( MLV_get_keyboard_state(MLV_KEYBOARD_q) == MLV_PRESSED )
-				monde->vaisseaux[0].dep = EST;
+				mo->vaisseaux[0].dep = EST;
 		case BOT:
 		case MIBOSS:
 		case BOSSFINALE:
-			i_vaisseau = monde->tab[y][x].indice;
-			if ( monde->vaisseaux[i_vaisseau].dep != STOP ) {
-				if ( peut_se_deplacer(monde, x, y, i_vaisseau) && !(*a_bouge) ) {
-					deplacer_vaisseau(monde, x, y);
-					monde->vaisseaux[i_vaisseau].dep = STOP;
+			i_vaisseau = mo->tab[y][x].indice;
+			if ( mo->vaisseaux[i_vaisseau].dep != STOP ) {
+				if ( peut_se_deplacer(mo, x, y, i_vaisseau) && !(*a_bouge) ) {
+					deplacer_vaisseau(mo, x, y);
+					mo->vaisseaux[i_vaisseau].dep = STOP;
 					*a_bouge = 1;
 				}
 			}
 			break;
+		default: break;
+	}
+}
+
+static void bonus_monde(Monde *mo, const int x, const int y) {
+	assert(NULL != mo);
+	assert(x >= 0);
+	assert(x < mo->taille_x);
+	assert(y >= 0);
+	assert(y < mo->taille_y);
+	switch(mo->tab[y][x].etats) {
 		/* Si c'est un bonus. */
 		case BONUS1:
 			break;
@@ -55,21 +88,40 @@ static void action_element(Monde* monde, int x, int y, int* a_bouge) {
 			break;
 		case BONUS3:
 			break;
+
+		default: break;
 	}
+}
+
+static void action_element(Monde *mo, const int x, const int y, int *a_bouge) {
+	assert(NULL != mo);
+	assert(NULL != a_bouge);
+	assert(x >= 0);
+	assert(x < mo->taille_x);
+	assert(y >= 0);
+	assert(y < mo->taille_y);
+
+	if (mo->tab[y][x].etats <= OBSTACLE)
+		matiere_monde(mo, x, y);
+
+	if (mo->tab[y][x].etats > OBSTACLE && mo->tab[y][x].etats <= TIR) 
+		tir_monde(mo, x, y);
+				
+	if (mo->tab[y][x].etats > TIR && mo->tab[y][x].etats <= BOSSFINALE)
+		vaisseaux_monde(mo, x, y, a_bouge);
+
+	if (mo->tab[y][x].etats > BOSSFINALE)
+		bonus_monde(mo, x, y);
 }
 
 void jouer(int taille_x, int taille_y) {
 	int x, y;
-	int a_bouge;
 	Monde monde;
-
+	int a_bouge;
 	initialiser_monde(&monde, taille_x, taille_y);
-
 	while ( 1 ) {
-
-		afficher_background();
 		a_bouge = 0;
-
+		afficher_background();
 		/* On parcours tous le tableau, et selon l'élément de la case, on effectue une action. */
 		for (y = 0; y < monde.taille_y; y++) {
 			for (x = 0; x < monde.taille_x; x++) {
@@ -77,7 +129,7 @@ void jouer(int taille_x, int taille_y) {
 				action_element(&monde, x, y, &a_bouge);
 			}
 		}
-
 		MLV_actualise_window();
 	}
+
 }
